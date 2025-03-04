795b93ef4a523dd37f977e69f9fa765df5aa7337
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-04 10:27:18 +0300

    Varribles

--- intro.js
+++ intro.js
@@ -11,4 +11,28 @@ console.log(59 + 30);
 console.log(20 + (22 - 5));
 console.log("Hello World");
 
-
+("Vincent Kimani");
+// name is an example of a varriable
+// We should use the camelcase naming cinvention ==firstName 
+name = "Vincent Kimani";
+console.log(name);
+//  age = "19";
+// console.log(age);
+num = 3 + 6;
+console.log(num);
+fullName = "Vincent Macharia Kimani";
+console.log(fullName);
+/* Rules When Creating varriables in Js
+1.We should use camelCase naming convention
+2.Js is case sensitive
+3.Don't start varriables with numbers, always start with lower case letter
+4.Do not use reserved words
+5.As a rule of thumb always use const or let to create varriables, there is also
+   the var keyword but we do not use it
+*/
+let age;
+age = 89;
+// let is used to reassign varriables
+console.log(age);
+age = 30;
+console.log(age);
