f612fd3c718dc04e811ef561c0f7de7df102026f
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-03-08 02:08:31 +0300

    Boolean Expression

--- intro.js
+++ intro.js
@@ -1,11 +1,11 @@
 console.log(2 / 8);
-console.log(59 + 30); 
+console.log(59 + 30);
 
 //single line comment
 /**
  * Multiple line comment
- * 
- * 
+ *
+ *
  */
 
 console.log(20 + (22 - 5));
@@ -13,7 +13,7 @@ console.log("Hello World");
 
 ("Vincent Kimani");
 // name is an example of a varriable
-// We should use the camelcase naming cinvention ==firstName 
+// We should use the camelcase naming cinvention ==firstName
 name = "Vincent Kimani";
 console.log(name);
 //  age = "19";
@@ -37,12 +37,11 @@ console.log(age);
 age = 30;
 console.log(age);
 
-
-const firstName = "Lionel"
+const firstName = "Lionel";
 console.log(typeof firstName);
 const lastName = "Messi";
 console.log(typeof lastName);
-console.log(firstName + " " +lastName);
+console.log(firstName + " " + lastName);
 const studentFirstName = "Vincent";
 const studentMiddleName = "Macharia";
 const studentLastName = "Kimani";
@@ -55,13 +54,13 @@ console.log(typeof fullName);
 Number = 5000 - 8000;
 console.log(Number);
 console.log(typeof Number);
-const money= "$" + 60000;
+const money = "$" + 60000;
 console.log(money);
 console.log(typeof money);
 
 console.log(typeof 800);
 console.log(typeof -9090);
-console.log(typeof Math.sqrt(- 4));
+console.log(typeof Math.sqrt(-4));
 console.log(Math.sqrt(25));
 console.log(typeof Math.sqrt);
 console.log(typeof Math.sqrt(25));
@@ -70,44 +69,43 @@ console.log(typeof true);
 console.log(typeof false);
 
 const student = {
-   name: "Joseph",
-   class: "Web9",
-   studentEmail:"joseph@student.moringaschool.com",
-   age : "20",
-   regNumber:"SCT222-0131/ 2024",
-   isDisciplined:true ,  
-}
+  name: "Joseph",
+  class: "Web9",
+  studentEmail: "joseph@student.moringaschool.com",
+  age: "20",
+  regNumber: "SCT222-0131/ 2024",
+  isDisciplined: true,
+};
 console.log(student);
 
-const supraMk4 ={
-   carName: "Supra MK4",
-   engine: "2JZ-series",
-   accelration: "100 km/h in 4.9 sec",
-   topSpeed: " 277km/h",
-   horsePower:"220 HP",
-}
+const supraMk4 = {
+  carName: "Supra MK4",
+  engine: "2JZ-series",
+  accelration: "100 km/h in 4.9 sec",
+  topSpeed: " 277km/h",
+  horsePower: "220 HP",
+};
 console.log(supraMk4);
 
-const cars = ["gtr", "skyLine", "supra"]
-const value = ["$" + 5000, "$"+6000, "$"+80000]
+const cars = ["gtr", "skyLine", "supra"];
+const value = ["$" + 5000, "$" + 6000, "$" + 80000];
 console.log(cars + " " + value);
 console.log(typeof cars + " " + value);
 
-
-// Statements 
+// Statements
 // collection of data with key value pairs
 const car = {
-   engine: "V8",
-   color: "Black",
-   brand: "Toyota",
-   seatCapacity: "2",
-   owner: {
-      firstName: "David",
-      phone: "0701804607",
-      ["full name"]:"David Kiptum"
-   },
-   previousOwners:["Tony"],
-}
+  engine: "V8",
+  color: "Black",
+  brand: "Toyota",
+  seatCapacity: "2",
+  owner: {
+    firstName: "David",
+    phone: "0701804607",
+    ["full name"]: "David Kiptum",
+  },
+  previousOwners: ["Tony"],
+};
 
 console.log(car.seatCapacity);
 console.log(car.owner["full name"]);
@@ -115,22 +113,22 @@ console.log(car.brand);
 console.log(car["owner"]["phone"]);
 
 const student2 = {
-   name: "Kimani Macharia",
-   age: "19",
-   gender: "Male",
-   class: {
-      cohort: "SDF-FT13",
-      name: "Web9",
-      tm: {
-         name: "Nelson",
-         yearsOfexperience: "2",
-      },
-   numberOfstudents:"41",
-   classModule:"SD",
-   }
-}
+  name: "Kimani Macharia",
+  age: "19",
+  gender: "Male",
+  class: {
+    cohort: "SDF-FT13",
+    name: "Web9",
+    tm: {
+      name: "Nelson",
+      yearsOfexperience: "2",
+    },
+    numberOfstudents: "41",
+    classModule: "SD",
+  },
+};
 console.log(student2.class.tm.name);
-console.log(student2['class']['tm']['yearsOfexperience']);
+console.log(student2["class"]["tm"]["yearsOfexperience"]);
 console.log(student2);
 console.log(`${student2.class.classModule},${student2.class.tm.name}`);
 // to check for a propery in an object:
@@ -150,11 +148,10 @@ console.log(Object.keys(student2));
 // 2. Object.values() -> returns an array of values
 console.log(Object.values(student2));
 
-const key = "engine"
+const key = "engine";
 console.log(car[key]);
 // smae thing as with 154 and 153
 console.log(car[`engine`]);
-
 
 // Functions allow reuserability
 // D.R.Y Do not repeat yourself
@@ -165,26 +162,26 @@ console.log(car[`engine`]);
 
 // syntax
 function functionName() {
-   // function body
+  // function body
 }
 
 console.log("Good morning Vincent");
 // Fuction Declaration
 function greeting(name) {
-   console.log(`Good morning ${name}`);
+  console.log(`Good morning ${name}`);
 }
 // call/ execute the function
 greeting("kimani");
 greeting("Anna");
 greeting("Joseph");
 
-function sum(x , y){
-   console.log(x + y);
+function sum(x, y) {
+  console.log(x + y);
 }
 sum(6, 5);
 sum(2, 2);
 
 function greetings(firstName, lastName) {
-   return `good morning ${firstName} ${lastName}`;
+  return `good morning ${firstName} ${lastName}`;
 }
-console.log(greetings('Macharia', 'Kimani'));
+console.log(greetings("Macharia", "Kimani"));
---
+++ prac.js
@@ -0,0 +1,24 @@
+/**
+ * Bolean expressions - expresssions that evaluate true and false
+ */
+console.log(1 + 1);
+console.log(2 === 2);
+console.log(2 === "2"); 
+
+console.log(2 !== 3);
+console.log(2 == "2"); 
+console.log(2 != 3);
+console.log(!true);
+console.log(!false); 
+
+console.log(Boolean());
+console.log(!!"Vincent Kimani");
+
+console.log(2 === 2 && 2 == "3");
+console.log(2 === 2 && (2 == "3" || 3 == 3));
+console.log(!!("Vincent" === "Anna"));
+
+const num1 = 50;
+const num2 = 30;
+console.log(num1 <= num2);
+console.log(40 >= 30);
